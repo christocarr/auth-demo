99c8b93ca4cce1775fcc5ec6f87864591bf24165
christocarr <christocarr@outlook.com>
2019-01-29 20:11:48 +0000

    Require dependancies

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const LocalStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true });
 
 const app = express();

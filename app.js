b572b6fd5854f092bb18328ee3d4bfbca62fdb07
christocarr <christocarr@outlook.com>
2019-01-29 20:00:06 +0000

    Add require mongoose

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true });
 
 const app = express();
 app.set('view engine', 'ejs');

69fe60228ff24c1e92b73c93e672fdcac69c18bd
christocarr <christocarr@outlook.com>
2019-01-29 19:48:54 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/' , (req, res) => {
+  res.render('home');
+});
+
+app.get('/secret', (req, res) => {
+  res.render('secret');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running...');
+});
